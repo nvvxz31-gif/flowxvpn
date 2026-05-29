a9cf687294ffed2d8c2eadc7396d61ea2bf91944
nvvxz31-gif <nvvxz31@github.com>
2026-05-29 13:10:52 -0700

    fix auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
+import { AuthProvider } from '@/lib/AuthContext';
 import { AppProvider } from '@/lib/AppContext';
 import MiniApp from './pages/MiniApp';
 import AdminPanel from './pages/AdminPanel';
@@ -13,17 +14,19 @@ function App() {
   return (
     <ErrorBoundary>
       <AppProvider>
-        <QueryClientProvider client={queryClientInstance}>
-          <Router>
-            <Routes>
-              <Route path="/" element={<MiniApp />} />
-              <Route path="/admin" element={<AdminPanel />} />
-              <Route path="/my/*" element={<UserPanel />} />
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
-            <Toaster />
-          </Router>
-        </QueryClientProvider>
+        <AuthProvider>
+          <QueryClientProvider client={queryClientInstance}>
+            <Router>
+              <Routes>
+                <Route path="/" element={<MiniApp />} />
+                <Route path="/admin" element={<AdminPanel />} />
+                <Route path="/my/*" element={<UserPanel />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+              <Toaster />
+            </Router>
+          </QueryClientProvider>
+        </AuthProvider>
       </AppProvider>
     </ErrorBoundary>
   )
